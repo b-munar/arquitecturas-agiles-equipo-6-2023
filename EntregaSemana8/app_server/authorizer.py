87ad234f54de4ec888598c24a0c4f1e70c7aa0cd
b-munar <b.munar@uniandes.edu.co>
2023-03-18 08:37:47 -0500

    se agrego mensajes de 403

--- EntregaSemana8/app_server/authorizer.py
+++ EntregaSemana8/app_server/authorizer.py
@@ -7,11 +7,13 @@ from flask_restful import abort
 def authenticate(func):
     @wraps(func)
     def decorated(*args, **kwargs):
+            if not func.__self__.argument["token_sign_in"]:
+                 return "No se provee el token", 403
             try:
                 token = func.__self__.argument["token_sign_in"]
                 data = jwt.decode(token, Config.SECRET_KEY, algorithms=["HS256"])
                 kwargs["user"] = User.query.get(data['public_id'])
                 return func(*args, **kwargs)
             except:
-                 abort(404)                 
+                 return "No tiene privilegios para acceder a este servico", 403                 
     return decorated
